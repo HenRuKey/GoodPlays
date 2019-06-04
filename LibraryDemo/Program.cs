ab64a303778b99df3ebe7987dc3a3c483b36714d
Michael Zopff <michael.zopff@gmail.com>
2019-06-03 19:45:24 -0600

    Add console app for demo on Wednesday

---
+++ LibraryDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using GoodPlaysEF.services;
+using GoodPlaysLib.controllers;
+using GoodPlaysLib.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemo {
+    class Program {
+        static void Main(string[] args) {
+            APIDemo();
+            DBDemo();
+        }
+
+        static void APIDemo() {
+            IGDBManager manager = new IGDBManager();
+
+            //Edit this
+            Game[] results = manager.SearchGamesByName("");
+        }
+
+        static void DBDemo() {
+            GoodPlaysEFService service = new GoodPlaysEFService();
+            List<string> userIDs = AddUsers(service);
+
+            //Edit this
+            service.AddListItemToUser(userIDs[0], 0, 0);
+
+            RemoveUsers(service, userIDs);
+        }
+
+        static List<string> AddUsers(GoodPlaysEFService service) {
+            List<string> results = new List<string>();
+            results.Add(service.CreateUser("Michael", "Zopff", "mzopff@student.neumont.edu"));
+            results.Add(service.CreateUser("Testy", "Testerson", "ttesty@example.com"));
+            return results;
+        }
+
+        static void RemoveUsers(GoodPlaysEFService service, List<string> userIDs) {
+            foreach (string userID in userIDs) {
+                service.DeleteListByUserID(userID);
+                service.DeleteUserByID(userID);
+            }
+        }
+    }
+}
